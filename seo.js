d5c9b6edce4e14aac4db2596ca2527ec161a9686
Cgun-art <migueloproio903@gmail.com>
2025-10-30 21:30:44 -0300

    Update seo.js

--- seo.js
+++ seo.js
@@ -1,1 +1,6 @@
- 
+document.addEventListener("DOMContentLoaded", function() {
+const metaDescription = document.querySelector("meta[name='description']");
+if (metaDescription && metaDescription.content.length > 150) {
+metaDescription.content = metaDescription.content.slice(0, 150);
+}
+}); 
