01385f930353a99a74d999ee4b992a26b6362a76
Cgun-art <migueloproio903@gmail.com>
2025-10-30 21:31:20 -0300

    Update seo.ts

--- seo.ts
+++ seo.ts
@@ -1,1 +1,6 @@
- 
+document.addEventListener("DOMContentLoaded", (): void => {
+const titleTag = document.querySelector("title");
+if (titleTag && titleTag.textContent) {
+titleTag.textContent = titleTag.textContent.trim();
+}
+}); 
